2204537e5c07837f4a83f1422e86a0e0f7c7497b
Torrence Brown <torrenceb90@gmail.com>
2023-06-15 14:10:59 -0600

    Init firebase & fetch todos collection from api

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,22 @@
-const express = require("express");
+import express from "express";
+import { db } from "./plugins/firebase.js";
+import { collection } from "firebase/firestore";
 
 const app = express();
 const port = 8080;
 
+const todosRef = collection(db, "todos");
+
+const fetchTodos = () => {
+  try {
+    console.log("Todos: ", todosRef);
+  } catch (err) {
+    throw new Error("An error has occurred: ", err);
+  }
+};
+
 app.get("/", (req, res) => {
+  fetchTodos();
   res.send("Todo app Server response");
 });
 
