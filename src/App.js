402bef2717649fea1e7e610fa64b73fb13a00b48
Torrence Brown <torrenceb90@gmail.com>
2023-06-19 15:42:26 -0600

    Fetch data from fe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import axios from "axios";
+
 function App() {
+  // GET localhost:8080/todos
+  const getTodos = async () => {
+    try {
+      const { data } = await axios({
+        method: "get",
+        url: "http://localhost:8080/todos",
+      });
+
+      console.log("Todos response: ", data);
+    } catch (err) {
+      throw new Error("An error has occurred:", err);
+    }
+  };
+
+  getTodos();
+
   return <div className="App">Test change</div>;
 }
 
